2690c92a8aa9afae68e8dc35d9ddae8c9d184d42
Divine <chukwubuikedivine27@gmail.com>
2025-10-27 15:04:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const readyToCommit = true;
+const readyToLearn =  true;
+let user1Commit = true;
+let user1Learn = true;
+
+if(user1Commit === readyToCommit && user1Learn === readyToLearn) {
+    console.log("User 1 is eligible to attend Gida bootcamp!");
+}else{
+    console.log("User 1 is not eligible to attend Gida bootcamp.");
+}
